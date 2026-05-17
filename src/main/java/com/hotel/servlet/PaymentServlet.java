48bd693b4cae9ef3e39d0b98085898a05ff6fad8
K.A.Rathmi Pabasara <it25100244@my.sliit.lk>
2026-05-17 13:30:57 +0530

    Update PaymentServlet.java

--- src/main/java/com/hotel/servlet/PaymentServlet.java
+++ src/main/java/com/hotel/servlet/PaymentServlet.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 
+
 @WebServlet("/payments")
 public class PaymentServlet extends HttpServlet {
     private PaymentDAO paymentDAO;
